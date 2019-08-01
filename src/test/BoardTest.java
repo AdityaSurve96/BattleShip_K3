b0dad0c7ea87d65e7783859e573dc7d932c2d9b2
Aditya Surve <aditya.surve2913@gmail.com>
2019-07-31 21:12:47 -0400

    added board  tests

--- src/test/BoardTest.java
+++ src/test/BoardTest.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,8 +18,8 @@ public class BoardTest {
 	static Cell cell;
 	
 	
-	@BeforeClass
-	public static void check() {
+	@Before
+	public  void check() {
 		board = new Board(false, null);
 	
 		cell = new Cell(3, 4,board);
@@ -25,11 +27,14 @@ public class BoardTest {
 	
 	}
 	
-	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@After
+	public  void clear() {
+		board =null;
+		cell = null;
 	}
+	
+	
+	
 	
 	@Test
 	public void IsValidPointTest()
@@ -39,6 +44,9 @@ public class BoardTest {
 		assertEquals(true, board.isValidPoint(p));
 	
 	}
+	
+	
+	@Test
 	public void IsInValidPointTest()
 	{
 		
@@ -46,12 +54,81 @@ public class BoardTest {
 		assertEquals(false, board.isValidPoint(p));
 	
 	}
+	
+	
+	@Test
 	public void IsInValidPointTest2()
 	{
 		
 		Point2D p = new Point2D(0, 20);
 		assertEquals(false, board.isValidPoint(p));
 	
+	}
+	
+	
+	@Test
+	public void testPositionShip() {
+		Ship s = new Ship(5, true);
+		
+		boolean placed = board.positionShip(s, 3, 5, false);
+		
+		assertTrue(placed);
+	}
+	
+	
+	@Test
+	public void testInvShipPos() {
+		Ship s = new Ship(5, true);
+		
+		boolean placed = board.positionShip(s, 20, 5, false);
+		
+		assertFalse(placed);
+	}
+	
+	@Test
+	public void testHorizShipPos() {
+		
+		Ship s = new Ship(5, false);
+		
+		boolean placed = board.positionShip(s, 5, 5, false);
+		
+		assertTrue(placed);
+	}
+	
+	
+	@Test
+	public void testInvHorizShipPos() {
+		
+		Ship s = new Ship(5, false);
+		
+		boolean placed = board.positionShip(s, 5, 12, false);
+		
+		assertFalse(placed);
+	}
+	
+	
+	@Test
+	public void testMovableShip() {
+		
+		Ship s = new Ship(5, false);
+		board.positionShip(s, 5, 4 , false);
+		
+		
+		boolean removed = board.positionShip(s, 5, 4, true);
+		
+		assertTrue(removed);
+	}
+	
+	@Test
+	public void testInvMovableShip() {
+		
+		Ship s1 = new Ship(3, false);
+		board.positionShip(s1, 3, 6 , false);
+		
+		
+		board.positionShip(s1, 5, 6, true);
+		
+		assertNull(cell.ship);
 	}
 	
 	
