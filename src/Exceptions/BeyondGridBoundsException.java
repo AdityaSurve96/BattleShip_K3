e4453e0261cf2a29dcd0f2347e2fc4157563036d
Rajasekhar Reddy <rajasekharchanti5599@gmail.com>
2019-08-08 20:28:49 -0700

    Added JavaDoc for BeyondGrid and DataNotSent

--- src/Exceptions/BeyondGridBoundsException.java
+++ src/Exceptions/BeyondGridBoundsException.java
@@ -1,5 +1,11 @@
 package Exceptions;
 
+
+/**
+ * This Custom Exception Class Handles All the Grid Bound Exceptions 
+ * @author K3
+ *
+ */
 public class BeyondGridBoundsException extends Exception{
 	public BeyondGridBoundsException(String s) {
 		super(s);
--- src/Exceptions/DataNotSentException.java
+++ src/Exceptions/DataNotSentException.java
@@ -1,5 +1,11 @@
 package Exceptions;
 
+/**
+ * This Custom Exception Class Handles UDP data sent between two Players.
+ * This calls the super exception to print the message.
+ * @author K3
+ *
+ */
 public class DataNotSentException extends Exception{
 	public DataNotSentException(String s) {
 		super(s);
