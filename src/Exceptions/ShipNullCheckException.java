cea8aae703703dfb839735945ab4f288241cd18f
Rajasekhar Reddy <rajasekharchanti5599@gmail.com>
2019-08-08 20:36:43 -0700

    Modified the Test Class and Added JavaDoc Comments

--- src/Exceptions/ShipNullCheckException.java
+++ src/Exceptions/ShipNullCheckException.java
@@ -1,5 +1,12 @@
 package Exceptions;
 
+/**
+ * <p>This Custom Exception Class Handles Null Ship Exception.
+ * If the ship is present at a particular cell or not .
+ * If not then the exception is raised.</p>
+ * @author K3
+ *
+ */
 public class ShipNullCheckException extends Exception{
 		public ShipNullCheckException(String s) {
 			super(s);
--- src/Exceptions/StartGameException.java
+++ src/Exceptions/StartGameException.java
@@ -1,5 +1,11 @@
 package Exceptions;
 
+/**
+ * <p>This Custom Exception Class Handles the player to player Game Scenario.
+ * If one player starts before the other player then this exception is thrown . </p> 
+ * @author K3
+ *
+ */
 public class StartGameException extends Exception {
 	public StartGameException(String s) {
 		super(s);
